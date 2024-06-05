2f79b2174be43c318eba872e1be5b732abe7fe34
Neha patankar <84632833+Neha-patankar@users.noreply.github.com>
2024-06-05 17:50:52 +0530

    important chancess

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Footer from './components/Footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+ return (
     <>
-      <h1>vhml;,</h1>
+      
+    <Footer/>
        
     </>
   )
